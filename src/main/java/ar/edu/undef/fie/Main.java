536b43a07abe585b45f29d235d1d53a687c1cab8
Hugo Cabaña <hcabana@fie.undef.edu.ar>
2023-09-11 08:57:22 -0300

    ejercicio resuelto

--- src/main/java/ar/edu/undef/fie/Main.java
+++ src/main/java/ar/edu/undef/fie/Main.java
@@ -1,19 +1,13 @@
 package ar.edu.undef.fie;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        var suma = 0;
+        for (var arg: args)
+            suma += Integer.parseInt(arg);
+        System.out.println("La suma es: " + suma);
+        double promedio = (double) suma / args.length;
+        System.out.println("El promedio es:  " + promedio);
     }
 }
